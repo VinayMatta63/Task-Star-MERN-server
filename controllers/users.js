278641b90ece197bc0b9ff644d3732465ab182d7
VinayMatta63 <vinaymatta63@gmail.com>
2021-10-10 10:31:50 +0530

    changed-signin-response

--- controllers/users.js
+++ controllers/users.js
@@ -34,6 +34,8 @@ module.exports.signupUser = async (req, res) => {
     jwt.sign(payload, "randomString", { expiresIn: "1d" }, (err, token) => {
       if (err) throw err;
       res.status(200).json({
+        email,
+        full_name,
         token,
       });
     });
